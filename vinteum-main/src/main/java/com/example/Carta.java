b897d121925a6f88c6bab4d33b8c112cfe7cb5f0
NicolasDobbeck <nicolasdobbeckmendes12@gmail.com>
2025-02-17 22:21:31 -0300

    Jogo vinte um feito em sala de aula

---
+++ vinteum-main/src/main/java/com/example/Carta.java
@@ -0,0 +1,30 @@
+package com.example;
+
+public class Carta {
+    private int numero;
+    private Naipe naipe;
+
+    public Carta(int numero, Naipe naipe) {
+        this.numero = numero;
+        this.naipe = naipe;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public Naipe getNaipe() {
+        return naipe;
+    }
+
+    public String imagePath() {
+        String prefixo = numero + "";
+        if (numero == 1) prefixo = "Ace";
+        if (numero == 11) prefixo = "Jack";
+        if (numero == 12) prefixo = "Queen";
+        if (numero == 13) prefixo = "King";
+
+        return "classic-cards/" + prefixo + naipe + ".png";
+    }
+
+}
